a6f5b45b81265c3f0fb362eb85326b1fa07bc90b
Joonghyeon Kim <jheloper9102@gmail.com>
2018-04-19 07:11:54 +0900

    #9 User를 JSON으로 출력하는 Request Handler 추가.

--- src/main/java/com/jhkim/springboot/controller/HelloController.java
+++ src/main/java/com/jhkim/springboot/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.jhkim.springboot.controller;
 
+import com.jhkim.springboot.model.DataObject;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,5 +22,13 @@ public class HelloController {
         }
 
         return "total : " + res;
+    }
+
+    @RequestMapping("/user/{id}")
+    public DataObject getUser(@PathVariable int id) {
+        String[] names = {"kim", "lee", "park", "choi", "cho"};
+        String[] mails = {"kim@test.com", "lee@test.com", "park@test.com", "choi@test.com", "cho@test.com"};
+
+        return new DataObject(id, names[id], mails[id]);
     }
 }
