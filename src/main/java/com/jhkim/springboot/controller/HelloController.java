f1c201a7ecd6b4d1a44b0ecd882bb3f30f7cae0b
Joonghyeon Kim <jheloper9102@gmail.com>
2018-04-20 22:56:17 +0900

    #10 Thymeleaf 템플릿 적용하여 index.html 출력하도록 수정.

--- src/main/java/com/jhkim/springboot/controller/HelloController.java
+++ src/main/java/com/jhkim/springboot/controller/HelloController.java
@@ -1,34 +1,13 @@
 package com.jhkim.springboot.controller;
 
-import com.jhkim.springboot.model.DataObject;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "Hello World!";
-    }
-
-    @RequestMapping("/{num}")
-    public String total(@PathVariable int num) {
-        int res = 0;
-
-        for (int i = 1; i <= num; i++) {
-            res += i;
-        }
-
-        return "total : " + res;
-    }
-
-    @RequestMapping("/user/{id}")
-    public DataObject getUser(@PathVariable int id) {
-        String[] names = {"kim", "lee", "park", "choi", "cho"};
-        String[] mails = {"kim@test.com", "lee@test.com", "park@test.com", "choi@test.com", "cho@test.com"};
-
-        return new DataObject(id, names[id], mails[id]);
+        return "index";
     }
 }
