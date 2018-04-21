c73791deca0ceb0a8d24a584c64ccffe01da15ba
Joonghyeon Kim <jheloper9102@gmail.com>
2018-04-21 23:27:36 +0900

    #11 템플릿에 1부터 URL로 전달받은 숫자까지의 총합을 출력하는 기능 추가.

--- src/main/java/com/jhkim/springboot/controller/HelloController.java
+++ src/main/java/com/jhkim/springboot/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.jhkim.springboot.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,5 +11,15 @@ public class HelloController {
     @RequestMapping("/")
     public String index() {
         return "index";
+    }
+
+    @RequestMapping("/{num}")
+    public String total(@PathVariable int num, Model model) {
+        int res = 0;
+        for (int i = 1; i <= num; i++) {
+            res += i;
+        }
+        model.addAttribute("msg", "total : " + res);
+        return "total";
     }
 }
