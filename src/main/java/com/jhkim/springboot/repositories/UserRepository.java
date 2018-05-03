ce454d5b673cc85d7956047102756b54e83ae750
Joonghyeon Kim <jheloper9102@gmail.com>
2018-05-03 23:21:39 +0900

    #16 User 엔티티와 리포지터리 추가.

---
+++ src/main/java/com/jhkim/springboot/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.jhkim.springboot.repositories;
+
+import com.jhkim.springboot.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
