75f396adb49b22f9ac565b878ce9e6531f2f8067
Joonghyeon Kim <jheloper9102@gmail.com>
2018-05-03 23:22:32 +0900

    #16 Rest 컨트롤러에 User 리포지터리를 이용하여 데이터를 반환하는 Request Handler 추가.

--- src/main/java/com/jhkim/springboot/controller/HelloRestController.java
+++ src/main/java/com/jhkim/springboot/controller/HelloRestController.java
@@ -1,6 +1,9 @@
 package com.jhkim.springboot.controller;
 
 import com.jhkim.springboot.model.DataObject;
+import com.jhkim.springboot.model.User;
+import com.jhkim.springboot.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,16 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloRestController {
 
-//    @RequestMapping("/{num}")
-//    public String total(@PathVariable int num) {
-//        int res = 0;
-//
-//        for (int i = 1; i <= num; i++) {
-//            res += i;
-//        }
-//
-//        return "total : " + res;
-//    }
+    @Autowired
+    UserRepository userRepository;
 
     @RequestMapping("/user/{id}")
     public DataObject getUser(@PathVariable int id) {
@@ -25,5 +20,12 @@ public class HelloRestController {
         String[] mails = {"kim@test.com", "lee@test.com", "park@test.com", "choi@test.com", "cho@test.com"};
 
         return new DataObject(id, names[id], mails[id]);
+    }
+
+    @RequestMapping("/users")
+    public Iterable<User> getUsers() {
+        Iterable<User> list = userRepository.findAll();
+
+        return list;
     }
 }
